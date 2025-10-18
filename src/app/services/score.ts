8e75f0c28cfe14f37fd5cd0a264fbeb4541f2c94
Nay Lin Htet <nlh2012.8@gmail.com>
2025-10-18 19:38:55 +1300

    fix: responsive

--- src/app/services/score.ts
+++ src/app/services/score.ts
@@ -5,7 +5,43 @@ import { ScoreElement, ScoreElementOnlyNecessary } from '../../ts/model/score.ty
   providedIn: 'root'
 })
 export class Score {
-  scores = signal<ScoreElement[]>([])
+  scores = signal<ScoreElement[]>([
+    // {
+    //   id: 1,
+    //   date: new Date(Date.now() - 5000000), // ~1.3 hours ago
+    //   yourChoice: 'ROCK',
+    //   computerChoice: 'PAPER',
+    //   result: 'Computer Wins!'
+    // },
+    // {
+    //   id: 2,
+    //   date: new Date(Date.now() - 3000000), // ~50 minutes ago
+    //   yourChoice: 'SCISSORS',
+    //   computerChoice: 'SCISSORS',
+    //   result: "It's a Tie!"
+    // },
+    // {
+    //   id: 3,
+    //   date: new Date(Date.now() - 1200000), // ~20 minutes ago
+    //   yourChoice: 'PAPER',
+    //   computerChoice: 'ROCK',
+    //   result: 'You Win!'
+    // },
+    // {
+    //   id: 4,
+    //   date: new Date(Date.now() - 60000), // ~1 minute ago
+    //   yourChoice: 'ROCK',
+    //   computerChoice: 'SCISSORS',
+    //   result: 'You Win!'
+    // },
+    // {
+    //   id: 5,
+    //   date: new Date(Date.now()), // Now
+    //   yourChoice: 'PAPER',
+    //   computerChoice: 'PAPER',
+    //   result: "It's a Tie!"
+    // }
+  ])
 
   addScore (score: ScoreElementOnlyNecessary) {
     this.scores.set([
